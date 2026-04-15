db20be19b2eb4fd8b8bf64ef61bd486ff91a07cd
erick <ericklimache14@gmail.com>
2026-04-15 11:42:02 -0500

    V1 PDO

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
-$conexion = mysqli_connect("localhost", "root", "", "bd_prueba");
-
-if (!$conexion) {
-    die("Error de conexión");
+try {
+    $conexion = new PDO("mysql:host=localhost;dbname=bd_prueba", "root", "");
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexión con PDO correcta";
+} catch (PDOException $e) {
+    echo "Error de conexión: " . $e->getMessage();
 }
-
-echo "Conexión con MySQLi correcta";
-?> 
+?>
